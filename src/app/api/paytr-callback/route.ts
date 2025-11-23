b4afac2ed2d8b016e02ebec50263d417fdf5df0e
etigkhn35-blip <etigkhn35@gmail.com>
2025-11-23 03:44:41 +0300

    Update route.ts

--- src/app/api/paytr-callback/route.ts
+++ src/app/api/paytr-callback/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import crypto from "crypto";
 import { db } from "@/lib/firebaseAdmin"; // Admin SDK bağlantısı
+export const runtime = "nodejs"; 
 
 export async function POST(req: Request) {
   try {
